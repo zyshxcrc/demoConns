271a3eeee017ba6e55e11ebb45f419b17c4e368a
zyliu2808 <891990586@qq.com>
2017-07-21 15:50:14 +0800

    修改servlet mvc异步调用

--- src/main/com.conn/controller/Upload.java
+++ src/main/com.conn/controller/Upload.java
@@ -4,29 +4,43 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.context.request.async.WebAsyncTask;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.AsyncContext;
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Administrator on 2017/7/21.
  */
 @Controller
 public class Upload {
+    private static final ExecutorService executor =  Executors.newFixedThreadPool(10);
     @RequestMapping("/doUpload")
-    public String doLogin(@RequestPart("file") MultipartFile file, HttpServletRequest request) throws Exception{
-        AsyncContext context = request.startAsync();
-        new Thread(() -> {
+    public DeferredResult<String> doUpload(final @RequestPart("file") MultipartFile file) throws Exception{
+        final DeferredResult<String> result = new DeferredResult<>(100*1000L);
+        executor.execute(() -> {
             try {
+                Thread.sleep(10*1000L);
                 file.transferTo(new File("D:\\ideawork\\demoConn\\src\\main\\webapp\\WEB-INF\\static\\img\\"+file.getOriginalFilename()));
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            context.complete();
-        }).start();
-        return "main";
+            result.setResult("main");
+        });
+        return result;
+    }
+
+    @RequestMapping("/doUpload1")
+    public Callable<String> doUpload1(){
+        return () -> "main";
+    }
+
+    @RequestMapping("/doUpload2")
+    public WebAsyncTask<String> doUpload2(){
+        return new WebAsyncTask<>(10 * 1000L, () -> "main");
     }
 }
--- src/main/com.conn/util/EncryptDecrypt.java
+++ src/main/com.conn/util/EncryptDecrypt.java
@@ -5,15 +5,14 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Created by Administrator on 2017/7/20.
+ * Created by liuziyang on 2017/7/20.
  */
 public class EncryptDecrypt {
-    private static String salt = "{PONY}";
     public static String doMD5Format(String str) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("MD5");
         digest.update(str.getBytes());
         byte[] mds = digest.digest();
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (byte b : mds) {
             sb.append(String.format("%02x",b));
         }
@@ -34,8 +33,9 @@ public class EncryptDecrypt {
     }
 
     public static void main(String[] args) throws Exception{
-        System.out.println(doMD5Format("000000"+salt));
-        System.out.println(doMD5Array("000000"+salt));
+        String salt = "{PONY}";
+        System.out.println(doMD5Format("000000"+ salt));
+        System.out.println(doMD5Array("000000"+ salt));
         System.out.println(URLEncoder.encode("jf2fkEAW5mYfbBQ6AHO+Bg==","UTF-8"));
         System.out.println(URLEncoder.encode("00000000000197","UTF-8"));
     }
